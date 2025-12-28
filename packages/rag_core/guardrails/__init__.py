1d85b8c9068fcf0e83943f15286de08bdcb5fbc2
Ruben Quispe <rubendqv@gmail.com>
2025-12-28 13:33:41 -0500

    Nuevos cambios

--- packages/rag_core/guardrails/__init__.py
+++ packages/rag_core/guardrails/__init__.py
@@ -4,12 +4,13 @@ Guardrails - Validación y seguridad de respuestas RAG
 
 from .grounding_check import GroundingChecker, GroundingResult
 from .pii_scrubber import PIIScrubber
-from .refusal_policy import RefusalPolicy, RefusalResult
+from .refusal_policy import RefusalPolicy, RefusalReason, RefusalResult
 
 __all__ = [
     "GroundingChecker",
     "GroundingResult",
     "RefusalPolicy",
+    "RefusalReason",
     "RefusalResult",
     "PIIScrubber",
 ]
